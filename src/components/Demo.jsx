591bb5c51af4defccc690f3abdbca682b7b0fefa
pocketcorp <pocketcorp@protonmail.com>
2025-01-13 22:59:47 -0800

    added copy button

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -10,6 +10,7 @@ const Demo = () => {
     })
 
     const [allArticles, setAllArticles] = useState([])
+    const [copied,setCopied] = useState('')
 
     const [getSummary, {error,isFetching}] = useLazyGetSummaryQuery()
 
@@ -31,6 +32,12 @@ const Demo = () => {
           console.log(newArticle)
           console.log(updatedAllArticles)
         }
+    }
+    
+    const handleCopy = (copyUrl)=>{
+      setCopied(copyUrl)
+      navigator.clipboard.writeText(copyUrl)
+      setTimeout(()=>setCopied(false),3000)
     }
 
   return (
@@ -65,8 +72,8 @@ const Demo = () => {
         <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
             {allArticles.map((item,index)=>(
               <div className="link_card" key={`link-${index}`} onClick={()=>setArticle(item)}>
-                <div className="copy_btn">
-                  <img src={copy} alt="copy_icon" className='w-[40%] h-[40%] object-contain'/>
+                <div className="copy_btn" onClick={()=>handleCopy(item.url)}>
+                  <img src={copied === item.url ? tick : copy} alt="copy_icon" className='w-[40%] h-[40%] object-contain'/>
                 </div>
                 <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
                 {item.url}
